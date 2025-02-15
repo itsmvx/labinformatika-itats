a06b83dcc06f259f09f5ec13e3e3e4e2d75f8794
Ismaindra <imismaindra@gmail.com>
2025-02-15 22:50:15 +0700

    update Aslab login page layout for full screen height

--- resources/js/Pages/Aslab/AslabLoginPage.tsx
+++ resources/js/Pages/Aslab/AslabLoginPage.tsx
@@ -111,7 +111,7 @@ export default function AslabLoginPage() {
     return (
         <>
             <Head title="Aslab - Login" />
-            <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
+            <div className="min-h-screen w-full dark:bg-black bg-white dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] flex items-center justify-center">
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
                 <div className="relative z-10 w-full max-w-6xl m-4 flex justify-between">
                     <div className="w-full max-w-md">
